675d3d0a418b917eaaa33b08db481894478b3caf
muratayu <toda2020-008@iniad.org>
2021-06-10 07:21:25 +0000

    add for sentence

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    uranai = random.choice(["大吉","中吉","小吉"])
-    return render_template("index.html",fortune=uranai)
+    unsei_list = ["大吉","中吉","小吉"]
+    uranai = random.choice(unsei_list)
+    return render_template("index.html",fortune=uranai,fortune_list=unsei_list)
 		
 if __name__ == "__main__":
     app.run(debug=True)
